a090724125554d016cc4423e82487a6c4ec63045
hobinhnguyen280106-collab <hobinhnguyen280106@gmail.com>
2026-09-13 18:56:06 +0700

    feat(dao): implement save method in EmployeeDAO

--- Slot_2/Chapter1-JPA-Basic/src/main/java/fe/DE200093/dao/EmployeeDAO.java
+++ Slot_2/Chapter1-JPA-Basic/src/main/java/fe/DE200093/dao/EmployeeDAO.java
@@ -1,4 +1,23 @@
 package fe.DE200093.dao;
 
+import fe.DE200093.pojo.Employee;
+import jakarta.persistence.*;
+
 public class EmployeeDAO {
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("hsf302FU");
+
+    // TODO 0.3: CREATE
+    public void save(Employee e) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(e);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            if (em.getTransaction().isActive()) em.getTransaction().rollback();
+            ex.printStackTrace();
+        } finally {
+            em.close();
+        }
+    }
 }
