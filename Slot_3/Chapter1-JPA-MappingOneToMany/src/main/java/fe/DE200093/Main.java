abb1ef8cfdedae70bb5a717e31c914b8839861e6
hobinhnguyen280106-collab <hobinhnguyen280106@gmail.com>
2026-09-15 17:05:40 +0700

    fix(dao): TODO 2.5 - standardize DAO layer with JPAUtil and clean up Main

--- Slot_3/Chapter1-JPA-MappingOneToMany/src/main/java/fe/DE200093/Main.java
+++ Slot_3/Chapter1-JPA-MappingOneToMany/src/main/java/fe/DE200093/Main.java
@@ -1,17 +1,38 @@
 package fe.DE200093;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import fe.DE200093.dao.DepartmentDAO;
+import fe.DE200093.pojo.Department;
+import fe.DE200093.pojo.Employee;
+import fe.DE200093.pojo.Gender;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // 1. Tạo EntityManagerFactory
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hsf302FU");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Department dept = new Department("Marketing", "Ha Noi");
+        Employee emp = new Employee("test2@company.com", "Test", Gender.OTHER,
+                new BigDecimal("1000"), LocalDate.now());
+        Employee e2 = new Employee("bc@company.com", "B", Gender.FEMALE,
+                new BigDecimal("1200"), LocalDate.of(2022, 2, 1));
+        Employee e3 = new Employee("cd@company.com", "C", Gender.OTHER,
+                new BigDecimal("1500"), LocalDate.of(2022, 3, 1));
+
+        dept.addEmployee(emp);
+        dept.addEmployee(e2);
+        dept.addEmployee(e3);
+
+        // 2. Truyền emf vào constructor
+        DepartmentDAO deptDAO = new DepartmentDAO(emf);
+        deptDAO.save(dept);
+        System.out.println("Đã thêm: " + dept.getName());
+
+        // 3. Đóng emf khi kết thúc
+        emf.close();
     }
 }
