2b81745b015bc62b373e539cfd7d9fab61ed60a5
hobinhnguyen280106-collab <hobinhnguyen280106@gmail.com>
2026-09-18 14:00:22 +0700

    feat(entity): TODO 5.1 - add Employee and Project entities

---
+++ Slot_4/Chapter1-JPA-MappingManyToMany/src/main/java/fe/DE200093/pojo/Project.java
@@ -0,0 +1,104 @@
+package fe.DE200093.pojo;
+
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(
+        name = "projects",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "projectCode")
+        }
+)
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String projectCode;
+
+    @Column(nullable = false)
+    private String projectName;
+
+    private BigDecimal budget;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    // Project là inverse side
+    // mappedBy = "projects" trỏ tới field projects trong Employee
+    @ManyToMany(mappedBy = "projects")
+    private Set<Employee> employees = new HashSet<>();
+
+    public Project() {
+    }
+
+    public Project(String projectCode, String projectName,
+                   BigDecimal budget, LocalDate startDate,
+                   LocalDate endDate) {
+        this.projectCode = projectCode;
+        this.projectName = projectName;
+        this.budget = budget;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getProjectCode() {
+        return projectCode;
+    }
+
+    public void setProjectCode(String projectCode) {
+        this.projectCode = projectCode;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public BigDecimal getBudget() {
+        return budget;
+    }
+
+    public void setBudget(BigDecimal budget) {
+        this.budget = budget;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public Set<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(Set<Employee> employees) {
+        this.employees = employees;
+    }
+}
